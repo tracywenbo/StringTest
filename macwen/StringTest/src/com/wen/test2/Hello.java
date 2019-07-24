273bdc1184f233365b7bff55b8ce98250ec7e2db
tracywenbo <macwen9107@163.com>
2019-07-24 11:57:44 +0800

    first git project

---
+++ macwen/StringTest/src/com/wen/test2/Hello.java
@@ -0,0 +1,12 @@
+package com.wen.test2;
+
+/**
+ * @Description:
+ * @Author 文博  88448045
+ * @Date 2019/7/24 11:56
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Tracy Macgrady");
+    }
+}
